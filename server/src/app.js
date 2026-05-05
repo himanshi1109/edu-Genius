cf1c5c7cebec94fc6a6d212b692c6e647b8d5c3b
Himanshi <himanshikhatri1109@gmail.com>
2026-05-05 21:24:04 +0530

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
   const buildPath = path.join(__dirname, 'client', 'dist');
   app.use(express.static(buildPath));
 
-  app.get('/(.*)', (req, res) => {
+  app.get('/:path*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 } else {
